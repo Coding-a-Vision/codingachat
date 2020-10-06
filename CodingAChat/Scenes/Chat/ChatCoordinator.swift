023fe6120cff4354d8069948874baf711f1cee9f
Samuele Rizzo <samuelerizzo97@outlook.com>
2020-10-06 11:32:35 +0200

    Implemented Chat UI

---
+++ CodingAChat/Scenes/Chat/ChatCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  ChatCoordinator.swift
+//  CodingAChat
+//
+//  Created by Samuele Francesco Rizzo on 06/10/2020.
+//  Copyright © 2020 CodingAVision. All rights reserved.
+//
+
+import Foundation
+
+class ChatCoordinator: Coordinator {
+    
+    var childCoordinators: [Coordinator] = []
+    
+    func start() {
+        //start
+    }
+    
+}
