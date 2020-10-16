666b8ad4705a88bd2c78e2a08e0c5236589ec152
Claudio Barbera <barbera.claudio@gmail.com>
2020-10-16 13:48:56 +0200

    improved apns

--- CodingAChat/Application/AppDelegate.swift
+++ CodingAChat/Application/AppDelegate.swift
@@ -28,6 +28,57 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         return true
     }
+
+    func registerForFCMNotifications() {
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, _ in
+            
+            if granted {
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
+        }
+    }
     
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        print("Token: \(fcmToken)")
+    }
+    
+    /*
+     
+     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+     
+         let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+         let token = tokenParts.joined()
+         print("Device Token: \(token)")
+         }
+         
+         func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+         print("Failed to register: \(error)")
+     } */
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        
+        // Print full message.
+        print(userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        // Print full message.
+        print(userInfo)
+        
+        if UIApplication.shared.applicationState == .active {
+            // Mostro semplicemente un alert all'utente, magari non bloccante
+        } else {
+            // se utente è loggato recupero il canale dal dizionario e provo a inviare l'utente nel dettaglio della chat
+        }
+        
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
 }
 
