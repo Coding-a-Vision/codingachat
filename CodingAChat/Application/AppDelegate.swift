c99ffa382bad03ea4bfb26abcccaedd3b52f1249
Claudio Barbera <barbera.claudio@gmail.com>
2020-10-16 12:26:51 +0200

    Register for push notifications

--- CodingAChat/Application/AppDelegate.swift
+++ CodingAChat/Application/AppDelegate.swift
@@ -25,9 +25,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         coordinator?.start()
         self.window = myWindow
         
+        // Register for push
+        registerForPushNotifications()
+        
         window?.makeKeyAndVisible()
         return true
     }
     
+    func registerForPushNotifications() {
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            } else if granted {
+                
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+                
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
 }
 
