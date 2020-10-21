c62c174137b1f58291054f87b8768919ffeb6187
Samuele Rizzo <samuelerizzo97@outlook.com>
2020-10-21 17:22:15 +0200

    Setup EditPassword Scene

---
+++ CodingAChat/Scenes/EditPassword/EditPasswordCoordinator.swift
@@ -0,0 +1,17 @@
+//
+//  EditPasswordCoordinator.swift
+//  CodingAChat
+//
+//  Created by Samuele Francesco Rizzo on 21/10/2020.
+//  Copyright © 2020 CodingAVision. All rights reserved.
+//
+
+import UIKit
+
+class EditPasswordCoordinator: Coordinator {
+    var childCoordinators: [Coordinator] = []
+    
+    func start() {
+        <#code#>
+    }
+}
---
+++ CodingAChat/Scenes/EditPassword/EditPasswordViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EditPasswordViewController.swift
+//  CodingAChat
+//
+//  Created by Samuele Francesco Rizzo on 21/10/2020.
+//  Copyright © 2020 CodingAVision. All rights reserved.
+//
+
+import UIKit
+
+class EditPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
